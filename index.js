90f47cfe6048bd7b1f62fcaac449930914c0e1b3
teknoreceh <danaputrayuda@gmail.com>
2023-03-24 01:14:07 +0700

    menggunakan require untuk manggil library http

---
+++ index.js
@@ -0,0 +1,5 @@
+// require, pakai  library untuk memanggil library bawaan JS
+// biasanya require itu untuk manggil library atau ngambil codingan di file lainnya 
+const http = require('http');
+console.log(http)
+// coba running dengan ketik node index.js
